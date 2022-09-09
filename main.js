ee88beeb5b9f35c673baed9d9b600974637c5a5b
ruancs <ruan.cs@hotmail.com>
2022-09-08 22:52:17 -0300

    testando js no vercel

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 import './style.scss'
 import RenderYoutube from "./js/app/RenderYoutube/VideoRender.js";
 import RegulamentoPopup from "./js/app/Regulamento/RegulamentoPopup.js";
-
 import FormValidate from "./js/app/RegisterSorteio/FormValidate"
 import ShowPopup from './js/app/ShowPopup/ShowPopup';
 import ScrollToArchor from './js/app/ScrollToArchor/ScrollToArchor'
-
 
 
 $(document).ready( function(){
